eaaef8bf8f46923e22afe573eeb19b640a90baec
Gustavo Henrique <Gustavo326projetos@gmail.com>
2023-12-15 21:42:38 -0300

    Finalizar footer

--- View/modules/Registro/RegistroForm.php
+++ View/modules/Registro/RegistroForm.php
@@ -14,6 +14,7 @@
     <link rel="stylesheet" href="../../../Assets/css/main.css">
     <link rel="stylesheet" href="../../../Assets/css/formRegister.css">
     <link rel="stylesheet" href="../../../Assets/css/tabelaRegistro.css">
+    <link rel="stylesheet" href="../../../Assets/css/footer.css">
     <title>Alphacode</title>
 </head>
 
@@ -106,6 +107,7 @@
             </table>
         </div>
     </main>
+    <footer></footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
