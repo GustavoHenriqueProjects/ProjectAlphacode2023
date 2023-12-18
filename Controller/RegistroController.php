2e274f44031f3723e8a9e6d60334f4fb2ac80f9e
Gustavo Henrique <Gustavo326projetos@gmail.com>
2023-12-18 09:03:18 -0300

    Finalizado readme 18/12/2023

--- Controller/RegistroController.php
+++ Controller/RegistroController.php
@@ -19,7 +19,6 @@ class RegistroController
         include './View/modules/Registro/RegistroForm.php';
     }
 
-    //Responsavel por registrar o usuario
     public static function save()
     {
 
@@ -27,7 +26,6 @@ class RegistroController
 
         $model = new RegistroModel();
 
-        // Preenche as propriedades do modelo com os dados do formulário
         $model->nome = $_POST['nome'];
         $model->data_nascimento = $_POST['data_nascimento'];
         $model->email = $_POST['email'];
@@ -38,7 +36,6 @@ class RegistroController
         $model->enviar_notificacao_sms = isset($_POST['enviar_notificacao_sms']) ? 1 : 0;
         $model->enviar_notificacao_email = isset($_POST['enviar_notificacao_email']) ? 1 : 0;
 
-        // Chama o método save do modelo para inserir os dados no banco de dados
         $model->save();
 
         echo '<script>window.location.href = "/";</script>';
@@ -48,32 +45,25 @@ class RegistroController
 
     public static function patch()
     {
-        // Obtenha os dados do corpo da requisição
         $data = json_decode(file_get_contents("php://input"), true);
 
-        // Obtém o ID da pessoa
         $id = $data['id'] ?? null;
 
-        // Verifica se o ID está presente
         if ($id !== null) {
             include './Model/RegistroModel.php';
             $model = new RegistroModel();
 
-            // Preenche as propriedades do modelo com os dados do formulário
             $model->pessoa_id = $id;
             $model->nome = $data['nome'] ?? null;
             $model->data_nascimento = $data['dataNascimento'] ?? null;
             $model->email = $data['email'] ?? null;
             $model->numero_celular = $data['numero_celular'] ?? null;
 
-            // Chama o método update do modelo para atualizar os dados no banco de dados
             $model->patch();
 
-            // Retorna uma resposta indicando sucesso
             echo json_encode(['success' => true]);
             exit();
         } else {
-            // Handle invalid or missing ID
             echo json_encode(['error' => 'Invalid or missing ID']);
             exit();
         }
@@ -81,7 +71,6 @@ class RegistroController
 
     public static function delete()
     {
-        // Verifique se o ID está presente na solicitação DELETE
         $data = json_decode(file_get_contents("php://input"), true);
         $id = $data['id'] ?? null;
 
@@ -89,16 +78,13 @@ class RegistroController
             include './Model/RegistroModel.php';
             $model = new RegistroModel();
 
-            // Set the ID in the model and call the delete method
             $model->pessoa_id = $id;
             $model->delete();
 
-            // Optionally, redirect or send a response indicating success
             echo '<script>window.location.href = "/";</script>';
 
             exit();
         } else {
-            // Handle invalid or missing ID
             echo json_encode(['error' => 'Invalid or missing ID']);
             exit();
         }
